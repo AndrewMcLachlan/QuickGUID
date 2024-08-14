67fe315dbda9ab003c728130022452ef86202116
Andrew McLachlan <andy@mclachlan.family>
2024-08-14 23:41:46 +1000

    Added alternative methods of generating a GUID.

---
+++ QuickGUID.NET/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+[DllImport("user32.dll")]
+static extern bool OpenClipboard(IntPtr hWndNewOwner);
+
+[DllImport("user32.dll")]
+static extern bool CloseClipboard();
+
+[DllImport("user32.dll")]
+static extern bool SetClipboardData(uint uFormat, IntPtr data);
+
+OpenClipboard(IntPtr.Zero);
+var guidString = Guid.NewGuid().ToString("D").ToLowerInvariant();
+var ptr = Marshal.StringToHGlobalUni(guidString);
+SetClipboardData(13, ptr);
+CloseClipboard();
+Marshal.FreeHGlobal(ptr);
